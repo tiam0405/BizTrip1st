1dfb3ac47149c6b161498e543fe3856b41385ec1
혜박 <chr0918@gmail.com>
2018-06-23 14:49:00 +0900

    test

---
+++ app/src/main/java/first/sample/biztrip/biztrip1st/MainActivity.java
@@ -0,0 +1,13 @@
+package first.sample.biztrip.biztrip1st;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
